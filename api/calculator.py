42c31622b14b7c641532759a3aa711e9d3c59b91
Steph <stephanemassina@gmail.com>
2025-08-06 17:44:29 +0200

    Développement API terminé

---
+++ api/calculator.py
@@ -0,0 +1,6 @@
+val_1 = int(input("Veillez saisir la première valeur"))
+val_2 = int(input("Veillez saisir la deuxième valeur"))
+
+resultat = val_1 * val_2
+
+print(resultat)
--- api/test_query_helpers.py
+++ api/test_query_helpers.py
@@ -8,4 +8,10 @@ movies = get_movies(db, limit=5)
 for movie in movies:
     print(f"Id: {movie.movieId}, Title: {movie.title}, Genres: {movie.genres}")
 
+
+ratings = get_ratings(db, limit=5, min_rating=3.0)
+
+for rating in ratings:
+    print(f"IdUser: {rating.userId}, IdMovie: {rating.movieId}, Note: {rating.rating}")
+
 db.close()
